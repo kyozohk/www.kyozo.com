6594de9772e7986dffa7b7c5575b401513b577cf
aeropriest <ashok.jaiswal@gmail.com>
2026-05-27 16:23:12 +0800

    * Updated the toke

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.variable}>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider firebase={getFirebase()}>
           <CommunityAuthProvider>
